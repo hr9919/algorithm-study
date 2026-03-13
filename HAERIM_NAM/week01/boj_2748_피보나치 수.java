e5bcf9a9f7030741d40f26f219dad3cd29cc45fe
hr9919 <hrnam99@chungbuk.ac.kr>
2026-03-14 02:43:06 +0900

    (Melting-Studio-Study#1) week01: BOJ 10828 스택, week01: BOJ 2748 피보나치수2

--- HAERIM_NAM/week01/boj_2748_피보나치 수.java
+++ HAERIM_NAM/week01/boj_2748_피보나치 수.java
@@ -2,23 +2,27 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
 
-    int n = Integer.parseInt(br.readLine());
-    if (n==0) {
-        System.out.println(0);
-        return;
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        long prev = 0;
+        long curr = 1;
+
+        for (int i = 2; i <= n; i++) {
+            long next = prev + curr;
+            prev = curr;
+            curr = next;
+        }
+
+        System.out.println(curr);
     }
-
-    long prev = 0;
-    long curr = 1;
-
-    for (int i = 2; i <= n; i++) {
-        long next = prev + curr;
-        prev = curr;
-        curr = next;
-
-    }
-
-    System.out.println(curr);
 }
